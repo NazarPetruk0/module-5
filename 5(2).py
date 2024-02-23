f842af520b22e3e61676a4be89595e911c975b84
NazarPetruk0 <154916004+NazarPetruk0@users.noreply.github.com>
2024-02-24 01:50:20 +0200

    Update 5(2).py

--- 5(2).py
+++ 5(2).py
@@ -1,7 +1,8 @@
 import re
 from typing import Callable
+
 def generator_numbers(text: str):
-    pattern = r'\b\d+\.\d+\b'
+    pattern = r'\s+\d+\.\d+\s+'
     for match in re.finditer(pattern, text):
         yield float(match.group())
 
@@ -10,6 +11,6 @@ def sum_profit(text: str, func: Callable):
     total_sum = sum(numbers_generator)
     return total_sum
 
-text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
+text = " 20.12 or 12.28, and 333.1 or 1.1 "
 total_income = sum_profit(text, generator_numbers)
 print(f"Загальний дохід: {total_income}")
